68ace4be9fe529008fa00883ed7b4df77aabd745
alvanrahimli <alvan@rahim.li>
2022-09-03 01:06:03 +0400

    Extension method to get User's ID

---
+++ src/Api/CardSystem.Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace CardSystem.Api.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var idStr = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        return int.Parse(idStr ?? Guid.Empty.ToString());
+    }
+}
