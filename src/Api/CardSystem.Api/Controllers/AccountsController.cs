6a2c124b26e0084e162278aec4e41525f540d000
alvanrahimli <alvan@rahim.li>
2022-09-03 01:06:26 +0400

    Added endpoint to get User's all accounts

---
+++ src/Api/CardSystem.Api/Controllers/AccountsController.cs
@@ -0,0 +1,32 @@
+﻿using CardSystem.Api.Extensions;
+using CardSystem.Api.Messages;
+using CardSystem.DataAccess.Abstract;
+using CardSystem.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardSystem.Api.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class AccountsController : ControllerBase
+{
+    private readonly IAsyncEntityRepository<Account, int> _accountRepository;
+    private readonly ILogger<AccountsController> _logger;
+
+    public AccountsController(IAsyncEntityRepository<Account, int> accountRepository,
+        ILogger<AccountsController> logger)
+    {
+        _accountRepository = accountRepository;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AccountMessage>>> GetAccounts()
+    {
+        var userId = User.GetUserId();
+        var accounts = await _accountRepository.GetAllAsync(a => a.UserId == userId);
+        return Ok(accounts.Select(a => new AccountMessage(a.Id, a.Balance, a.Type.ToString())));
+    }
+}
---
+++ src/Api/CardSystem.Api/Messages/AccountMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace CardSystem.Api.Messages;
+
+public record AccountMessage(
+    int Id,
+    decimal Balance,
+    string Type);
